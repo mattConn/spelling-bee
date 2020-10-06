94acef7f91b3b7ac689ab3697191184401b20e77
Matthew Connelly <matthew.b.connelly@gmail.com>
2020-10-06 18:19:53 -0400

    adding and deleting letters from current word

--- src/index.js
+++ src/index.js
@@ -1,17 +1,73 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+
+class Letter extends React.Component {
+    render() {
+      return (
+        <button className="letter" onClick={this.props.onClick}>
+          {this.props.value}
+        </button>
+      );
+    }
+ }
+
+ class App extends React.Component {
+   constructor(props){
+     super(props);
+
+      this.letters = ['r','i','o','y','f','l'];
+      this.centerLetter = 'g';
+
+     this.state = {
+       currentWord: '',
+       foundWords: [],
+       possibleWords: []
+     }
+
+
+     this.letterComponents = this.letters.map(
+       (letter,index) => this.renderLetter(letter,index)
+       );
+   }
+
+     letterClickHandler(letter)
+     {
+       this.setState({currentWord: this.state.currentWord+letter});
+     }
+
+    enterHandler(){
+      // clear old word
+      this.setState({currentWord: ''});
+    }
+
+    deleteHandler(){
+      // remove last letter from current word
+      this.setState({currentWord: this.state.currentWord.slice(0,-1)});
+    }
+
+   renderLetter(letter,key){
+     return <Letter key={key} value={letter} onClick={()=>this.letterClickHandler(letter)}/>
+   }
+
+   render(){
+     return (
+       <div>
+         <div>{this.state.currentWord}</div>
+        {this.letterComponents}
+        {this.renderLetter(this.centerLetter,'center')}
+        <div>
+       <button className="shuffle">Shuffle</button>
+       <button className="delete" onClick={()=>this.deleteHandler()}>Delete</button>
+       <button className="Enter" onClick={()=>this.enterHandler()}>Enter</button>
+       </div>
+       </div>
+     );
+   }
+ }
+
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <App />,
+    document.getElementById('root')
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
